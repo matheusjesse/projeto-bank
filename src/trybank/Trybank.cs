a8a3a5f94124e30eec497a39c7003acec7c518b7
matheusjesse <matheus_jesse@hotmail.com>
2023-10-16 06:45:10 -0300

    feat: criando a funcionalidade de transferir dinheiro entre contas

--- src/trybank/Trybank.cs
+++ src/trybank/Trybank.cs
@@ -106,7 +106,23 @@ public class TrybankLib
     // 7. Construa a funcionalidade de transferir dinheiro entre contas
     public void Transfer(int destinationNumber, int destinationAgency, int value)
     {
-        throw new NotImplementedException();
+        if (!Logged) throw new AccessViolationException("Usuário não está logado");
+        if(value > Bank[loggedUser, 3])
+        {
+            throw new InvalidOperationException("Saldo insuficiente");
+        }             
+                
+        for(int i = 0; i < registeredAccounts; i++)
+        {
+            if(Bank[i, 0] == destinationNumber && Bank[i, 1] == destinationAgency)
+            {
+                int newLoggedUserBalance = Bank[loggedUser, 3] - value;
+                Bank[loggedUser, 3] = newLoggedUserBalance;   
+
+                int newBalance = Bank[i, 3] + value;
+                Bank[i, 3] = newBalance;
+            }
+        }
     }
 
    
